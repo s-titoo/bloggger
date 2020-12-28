cb5e2f6a3868500eb24267c197b86882e6fbfc52
Sasha <sanyatito@gmail.com>
2020-12-29 00:02:45 +0100

    Change app structure to distinguish reusable elements
     - Split app into auth, error handling, and core modules

--- app/translate.py
+++ app/translate.py
@@ -1,41 +1,18 @@
 import json
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
 
 def translate(text, source_language, dest_language):
-
-    endpoint = 'https://api.cognitive.microsofttranslator.com'
-    path = '/translate?api-version=3.0'
-    params = '&from={}&to={}'.format(source_language, dest_language)
-    url_to_pass = endpoint + path + params
-
-
-    # add 'Ocp-Apim-Subscription-Region': 'westeurope'
-    # if for your resource you chose region other than Global
-    
-    headers = {
-    'Content-type': 'application/json',
-    'Content-Length': '10000',
-    # 'Ocp-Apim-Subscription-Region': 'westeurope'
-    }
-
-    body = [{
-    'text' : text
-    }]
-
-    if 'MS_TRANSLATOR_KEY' not in app.config or \
-    not app.config['MS_TRANSLATOR_KEY']:
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    headers['Ocp-Apim-Subscription-Key'] = app.config['MS_TRANSLATOR_KEY']
-
-    r = requests.post(url_to_pass, headers=headers, json=body)
-
-    # return r.status_code, headers
-
+    auth = {'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+    r = requests.post(
+        'https://api.cognitive.microsofttranslator.com'
+        '/translate?api-version=3.0&from={}&to={}'.format(
+            source_language, dest_language), headers=auth, json=[
+                {'Text': text}])
     if r.status_code != 200:
         return _('Error: the translation service failed.')
-    parsed_json = json.loads(r.content.decode('utf-8-sig'))
-    translated_text = parsed_json[0]['translations'][0]['text']
-    return translated_text
-
+    return r.json()[0]['translations'][0]['text']
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
-
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -12,12 +15,11 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = [os.environ.get('ADMINS')][0]
-
+    ADMINS = os.environ.get('ADMINS')[0]
+    
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-
+    
     POSTS_PER_PAGE = 5
     LANGUAGES = ['en', 'pl', 'uk', 'ru']
